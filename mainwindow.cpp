397a8d1cc09c35608bb6475055dd2dfa944cd6dd
Tarmo Johannes <tarmo@otsakool.edu.ee>
2016-09-18 17:44:28 +0300

    Added some buttons and csdPlainTextEdit. Htmlview disappeared...

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
+#include <QFileDialog>
 
 
 
@@ -9,14 +11,18 @@ mainWindow::mainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    QLabel * label = new QLabel("UUS");
+
+
     view = new QWebEngineView();
-    view->setUrl(QUrl("qrc:/index.html"));
-    view->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    view->setUrl(QUrl(":/index.html"));
+    //view->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     ui->verticalLayout->addWidget(view);
     //view->resize(400, 500);
+    ui->verticalLayout->addWidget(label);
 
-
-
+    csd = ":/test.csd";
+    loadFile();
     //cs = new CsoundWrapper();
     view->page()->setWebChannel(&channel);
     channel.registerObject("csound", &cs) ;
@@ -38,21 +44,46 @@ void mainWindow::stateChanged(int state)
 }
 
 
-void mainWindow::on_pushButton_clicked()
-{
+//void mainWindow::on_pushButton_clicked()
+//{
 
-    double value = cs.getChannel("testChannel");
-    qDebug()<<"request channel value" << value;
-    QString command = QString("document.getElementById( \"label\").innerHTML = \"%1\" ").arg(value); //QString("newChannelValue(%1,%2,%3,'%4');").arg(sX).arg(sY).arg(sz).arg(colour) ;
-    view->page()->runJavaScript(command) ;
-}
+//    double value = cs.getChannel("testChannel");
+//    qDebug()<<"request channel value" << value;
+//    QString command = QString("document.getElementById( \"label\").innerHTML = \"%1\" ").arg(value); //QString("newChannelValue(%1,%2,%3,'%4');").arg(sX).arg(sY).arg(sz).arg(colour) ;
+//    view->page()->runJavaScript(command) ;
+//}
 
 void mainWindow::on_playButton_clicked()
 {
-    cs.play(":/test.csd"); // TODO: qrc
+    cs.play(csd);
 }
 
 void mainWindow::on_stopButton_clicked()
 {
     cs.stop();
+}
+
+void mainWindow::on_loadButton_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"),
+                                                    "/home/tarmo/tarmo/csound",
+                                                    tr("Csound files (*.csd)"));
+    if (fileName.endsWith(".csd")) {
+        csd = fileName;
+        loadFile();
+    }
+}
+
+void mainWindow::loadFile()  {
+    QFile csdFile(csd);
+    if (!csdFile.open(QIODevice::ReadOnly | QIODevice::Text) ) {
+        qDebug()<< "Could not open "<<csd;
+    }
+    ui->csdTextEdit->document()->setPlainText(csdFile.readAll() );
+
+}
+
+void mainWindow::on_updateButton_clicked()
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -23,23 +23,29 @@ public:
     explicit mainWindow(QWidget *parent = 0);
     ~mainWindow();
 
+    void loadFile();
 public slots:
     void stateChanged(int state);
 
 private slots:
 	//void cursorMoved(int x, int y) ;
 
-	void on_pushButton_clicked();
+//	void on_pushButton_clicked();
 
     void on_playButton_clicked();
 
     void on_stopButton_clicked();
+
+    void on_loadButton_clicked();
+
+    void on_updateButton_clicked();
 
 private:
     Ui::mainWindow *ui;
     CsoundWrapper cs; // NB! must be in stack, ie not pointer...
     QWebChannel channel ;            // Channel for C++ to Javascript comms
     QWebEngineView * view;
+    QString csd;
 
 };
 
